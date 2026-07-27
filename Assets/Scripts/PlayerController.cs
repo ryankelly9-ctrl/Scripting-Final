8d1e5dde5aeca6f5bde4197c413c74608e9263b4
ryankelly9-ctrl <ryan.kelly9@g.austincc.edu>
2026-07-26 21:12:31 -0500

    player can now move
    player movement implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    private float verticalInput;
+    private float horizontalInput;
+
+    public bool isDead;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (!isDead)
+        {
+            HandleMovement();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * verticalInput * Time.deltaTime);
+        }
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.Translate(Vector3.right * moveSpeed * horizontalInput * Time.deltaTime);
+        }
     }
 }
