d19b33af895ecf185a6c136c166f4560ed1a1afb
ryankelly9-ctrl <ryan.kelly9@g.austincc.edu>
2026-07-28 20:20:49 -0500

    enemy script basics complete - now follows player. Player dropped and configured into each scene.
    now the enemy game object will automatically follow the player's position in real time. The speed can be adjusted, and most definitely will be depending on the enemy type, but we'll get to that later.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform player;
+    private NavMeshAgent navAgent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        navAgent.SetDestination(player.position);
     }
 }
